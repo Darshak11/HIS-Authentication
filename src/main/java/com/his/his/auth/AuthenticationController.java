18d68482b98a72af1cdc08a117eb18ac5c8959c7
KaranjitSaha <Karanjit.Saha@iiitb.ac.in>
2024-04-07 11:55:50 +0530

    Fixed CORS error

--- src/main/java/com/his/his/auth/AuthenticationController.java
+++ src/main/java/com/his/his/auth/AuthenticationController.java
@@ -3,12 +3,14 @@ package com.his.his.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController 
